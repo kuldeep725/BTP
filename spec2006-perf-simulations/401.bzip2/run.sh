7f055f82973fda1d2d0c7cbcbd0a440e6933fcf8
kuldeep725 <kunal725725@gmail.com>
2019-11-01 23:31:10 +0530

    Simulate spec-2006 programs

---
+++ spec2006-perf-simulations/401.bzip2/run.sh
@@ -0,0 +1,6 @@
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/bzip2_xinput1.csv -o yinputs/bzip2_input1.csv
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/bzip2_xinput2.csv -o yinputs/bzip2_input2.csv
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/bzip2_xinput3.csv -o yinputs/bzip2_input3.csv
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/bzip2_xinput4.csv -o yinputs/bzip2_input4.csv
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/bzip2_xinput5.csv -o yinputs/bzip2_input5.csv
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/bzip2_xinput6.csv -o yinputs/bzip2_input6.csv
---
+++ spec2006-perf-simulations/403.gcc/run.sh
@@ -0,0 +1,10 @@
+set -x
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/gcc_xinput1.csv -o yinputs/gcc_input1.csv
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/gcc_xinput2.csv -o yinputs/gcc_input2.csv
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/gcc_xinput3.csv -o yinputs/gcc_input3.csv
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/gcc_xinput4.csv -o yinputs/gcc_input4.csv
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/gcc_xinput5.csv -o yinputs/gcc_input5.csv
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/gcc_xinput6.csv -o yinputs/gcc_input6.csv
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/gcc_xinput7.csv -o yinputs/gcc_input7.csv
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/gcc_xinput8.csv -o yinputs/gcc_input8.csv
+$HOME/BTP/pmu-tools/interval-normalize.py xinputs/gcc_xinput9.csv -o yinputs/gcc_input9.csv
